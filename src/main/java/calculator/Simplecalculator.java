3a94fda9f804cc30b352c13897981c04f7501fda
Hoyoung Kim <kimtm60@naver.com>
2016-04-11 17:49:13 +0900

    어리석은 수정

--- src/main/java/calculator/Simplecalculator.java
+++ src/main/java/calculator/Simplecalculator.java
@@ -13,8 +13,6 @@ public class Simplecalculator {
 	
 	public void div(int x,int y){
 		if(y!=0) result = x/y;
-		else 
-			System.out.println("분모가 0입니다.");
 	}
 	public int getResult(){
 		return result;
